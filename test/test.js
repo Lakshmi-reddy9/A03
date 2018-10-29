f71eddf2e1dd5c46f58d85b8c8ebe7d514a74e06
Sai Sri Lakshmi Vancha <vanchalakshmi@gmail.com>
2018-10-28 19:24:13 -0500

    Modified app.js

---
+++ test/test.js
@@ -0,0 +1,7 @@
+QUnit.test('factorial()', function (assert) {
+
+    assert.equal(GuessNum(0), "You have selected Factorial function. The result for given number 0 is: 1", 'Tested with 0 and gives 1 ');
+    assert.equal(factorial(4), "You have selected Factorial function. The result for given number 4 is: 24", 'Tested with NUmber');
+    assert.equal()
+}
+);
